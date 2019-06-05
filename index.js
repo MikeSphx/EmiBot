c9e580d3a8489297a6d550eb8e3736c53a60b616
MikeSphx <kim.michae@husky.neu.edu>
2019-06-05 03:58:05 -0400

    Add more documentation to index file

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 // Load event handlers
+// Ready - Handles functionality tied to startup
 const readyHandler = require('./events/ready.js');
+// Message - Handles functioality tied to messages
 const messageHandler = require('./events/message.js');
 
+// Activate handlers
 client.on('ready', () => readyHandler(client));
 client.on('message', msg => messageHandler(client, msg));
 
